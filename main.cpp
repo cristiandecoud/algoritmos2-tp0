3b94eb5c6ee9558668f47069902237d5118b312a
Dogui96 <49008364+Dogui96@users.noreply.github.com>
2019-10-07 17:29:15 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,108 +1,199 @@
-#include <iostream>
 #include <cmath>
-#include <iomanip>
-#include <fstream>
 #include <string>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
+
 #include "complejo.h"
 #include "complejo.cpp"
 #include "array.h"
 #include "fourier.h"
 
+
+#include "cmdline.h"
+#include "cmdline.cc"
 using namespace std;
 
-static void
-saltar_lineas_blanco( ifstream &input_file )
-{
-    int c;
-    c = input_file.get();                   // 
-                                            //
-        while( c == '\n' || c == ' ' ) {    //
-            c = input_file.get();           // Para saltear lineas en blanco o llenas de espacios
-        }                                   //
-        input_file.putback(c);              //
 
+/**************** Prototipos ******************/
+static void opt_input(string const &);
+static void opt_output(string const &);
+static void opt_method(string const &);
+static void opt_help(string const &);
+
+
+/**************** Elementos globales ******************/
+static option_t options[] = {
+	{1, "i", "input", "-", opt_input, OPT_DEFAULT},
+	{1, "o", "output", "-", opt_output, OPT_DEFAULT},
+	{1, "m", "factor", "dft", opt_method, OPT_DEFAULT},
+	{0, "h", "help", NULL, opt_help, OPT_DEFAULT},
+	{0, },
+};
+
+static string method;
+static istream *iss = 0;	// Input Stream (clase para manejo de los flujos de entrada)
+static ostream *oss = 0;	// Output Stream (clase para manejo de los flujos de salida)
+static fstream ifs; 		// Input File Stream (derivada de la clase ifstream que deriva de istream para el manejo de archivos)
+static fstream ofs;		// Output File Stream (derivada de la clase ofstream que deriva de ostream para el manejo de archivos)
+
+
+
+/*****************************************************/
+
+static void
+opt_input(string const &arg)
+{
+	// Si el nombre del archivos es "-", usaremos la entrada
+	// est�ndar. De lo contrario, abrimos un archivo en modo
+	// de lectura.
+	//
+	if (arg == "-") {
+		iss = &cin;
+	}
+	else {
+		ifs.open(arg.c_str(), ios::in);
+		iss = &ifs;
+	}
+
+	// Verificamos que el stream este OK.
+	//
+	if (!iss->good()) {
+		cerr << "cannot open "
+		     << arg
+		     << "."
+		     << endl;
+		exit(1);
+	}
 }
 
 static void
-saltar_espacios( int &c ,ifstream &input_file )
+opt_output(string const &arg)
 {
-    
-    while ( c == ' ' ) {        //
-        c = input_file.get();   // Para que no rompa cuando lee espacios al final
-    } 
+	// Si el nombre del archivos es "-", usaremos la salida
+	// est�ndar. De lo contrario, abrimos un archivo en modo
+	// de escritura.
+	//
+	if (arg == "-") {
+		oss = &cout;
+	} else {
+		ofs.open(arg.c_str(), ios::out);
+		oss = &ofs;
+	}
+
+	// Verificamos que el stream este OK.
+	//
+	if (!oss->good()) {
+		cerr << "cannot open "
+		     << arg
+		     << "."
+		     << endl;
+		exit(1);
+	}
+}
+
+static void
+opt_method(string const &arg)
+{
+	if (arg == "dft"){
+		method = arg;
+	}else if(arg == "idft"){
+		method = arg;
+	}else{
+		cerr << "Comando '"<< arg << "' inválido - Solo se permite dft o idft"
+		     << endl;
+		exit(1);
+	}
+}
+
+static void
+opt_help(string const &arg)
+{
+	cout << "cmdline [-m method] [-i file] [-o file]"
+	     << endl;
+	exit(0);
 }
 
 
+static void fourier(istream *is, ostream *os) {
 
-int main()
-{
-    cout << fixed;      //Para que no imprima en notación científica.
-    cout.precision(2);  //Para imprimir con dos decimales.
-
-    ifstream input_file;
-    input_file.open("prueba.txt", ifstream::in);
-    
-
-    while( input_file ) {
+    (*os) << fixed;      //Para que no imprima en notación científica.
+    (*os).precision(2);  //Para imprimir con dos decimales.
+    while( *is ) {
 
         Array <Complejo> x; 
-        int c = input_file.get();
+        int c = is->get();
         int d;
         size_t i = 0;
         bool line_error = false;
 
         while( c != '\n'  && c != EOF ){
 
-            if(i>=x.getSize()){
-                //cout << "Tamaño inicial de x:" <<x.getSize() <<'\n';
+            if( i >= x.getSize() ){
                 x.expand(x.getSize() * 2);
-                //cout << "Tamaño final de x:" <<x.getSize() <<'\n';
             }
 
-            input_file.putback(c);
-            input_file >> x[i];
+            is->putback(c);
+            *is >> x[i];
 
-            if((input_file.rdstate() & ifstream::badbit) !=0){
+            if((is->rdstate() & ifstream::badbit) !=0){
                 
-                input_file.clear();
+                is->clear();
                 line_error = true;
                 i = 0;
-                c = input_file.get();
+                c = is->get();
                 while ( c != '\n' && c != EOF){
-                    c = input_file.get();
+                    c = is->get();
                 }
                 break;
             }
             i++;
 
-            c = input_file.get();
+            c = is->get();
 
-            saltar_espacios( c, input_file );                          //
+            while ( c == ' ' ) {
+                c = is->get();   // Para que no rompa cuando lee espacios al final
+            } 
             
 
         }
 
-        saltar_lineas_blanco( input_file );
+        c = is->get();               
+
+        while( c == '\n' || c == ' ' ) { 
+            c = is->get();           // Para saltear lineas en blanco o llenas de espacios
+        }                                   
+        is->putback(c);
 
         if( line_error ) {
-            cout << '\n' << "Error: los resultados de esta línea no son válidos."<<'\n';
+        	(*os) << "Error: los argumentos de esta línea no son válidos." <<endl;
             line_error = false;
             
         } else if( i == 0 ){}
         
         else {
-            Array <Complejo> y = DFT(x, i);
-            std::cout << '\n' << "\e[92m Resultado de la linea: \e[0m" << '\n';
+			Array <Complejo> y;
+
+			if( method == "dft")
+            	y = DFT(x, i);
+			else
+				y = IDFT(x, i);
 
             for( int k = 0; k < y.getSize(); k++ ) {
-                std::cout << y[k] << '\n';
-            
+                *os << y[k] << ' ';
             }
+			*os << endl;
         }        
     };
+}
 
-    input_file.close();
-    return 0;
+int main(int argc, char * const argv[])
+{
+    cmdline cmdl(options);	// Objeto con parametro tipo option_t (struct) declarado globalmente. Ver l�nea 51 main.cc
+	cmdl.parse(argc, argv); // Metodo de parseo de la clase cmdline
+    fourier(iss, oss);
 }
 
 
